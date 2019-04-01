41e685f2207d0e17a1c5a494d540b1d03a2462fc
Steeven9 <stefano.taille@gmail.com>
2019-04-01 22:43:12 +0200

    fixed print statements

--- mergeSort.py
+++ mergeSort.py
@@ -1,5 +1,6 @@
 def mergeSort(A):
     if len(A) > 1:
+        print(A)
         mid = len(A) // 2
         lefthalf = A[:mid]
         righthalf = A[mid:]
@@ -19,18 +20,21 @@ def mergeSort(A):
                 A[k]=righthalf[j]
                 j += 1
             k += 1
+            print(A)
 
         while i < len(lefthalf):
             A[k]=lefthalf[i]
             i += 1
             k += 1
+            print(A)
 
         while j < len(righthalf):
             A[k]=righthalf[j]
             j += 1
             k += 1
+            print(A)
     return A
 
 A = [5, 6, 12, 8, 4, 10, 3, 12, 11, 1]
 
-print(mergeSort(A))
+mergeSort(A)
